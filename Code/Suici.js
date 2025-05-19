7b0616faa3acaea60a1fe7bb6e86325703ccae7e
zerosaid <daniel235ub@gmail.com>
2025-05-19 15:43:18 -0500

    fix: :bug: Cambio de mensajes
    Ahora todos los mensaje sson ventanas emergentes lo que ayuda a la lectura de las respuestas

--- Code/Suici.js
+++ Code/Suici.js
@@ -18,32 +18,32 @@ do {
 
       switch (opc2) {
         case 1:
-          console.log("El arma se encasquillo");
+          alert("El arma se encasquillo");
         case 2:
-          console.log("La cuerda se rompio");
+          alert("La cuerda se rompio");
         case 3:
-          console.log("Las pastillas estan caducadas");
+          alert("Las pastillas estan caducadas");
           break;
         case 4:
-          console.log("Esto no es odjeto o pastel, intenta otra cosa");
+          alert("Esto no es odjeto o pastel, intenta otra cosa");
         case 5:
-          console.log("No hay gasolina en todo el pais es una pena, no te atropellaron.");
+          alert("No hay gasolina en todo el pais es una pena, no te atropellaron.");
         case 6:
-          console.log("Justo paso un camion de colchones y te salvaste de milagro, te dejaron en una gasolinera cercana y te dieron una rosquilla");
+          alert("Justo paso un camion de colchones y te salvaste de milagro, te dejaron en una gasolinera cercana y te dieron una rosquilla");
         case 7:
-          console.log("Chebre ahora vete");
+          alert("Chebre ahora vete");
           break;
         default:
-          console.log("Elige una de las opciones presentadas.");
+          alert("Elige una de las opciones presentadas.");
       }
       break;
     case 2:
-      console.log("Buena elección, que tengas un gran día.");
+      alert("Buena elección, que tengas un gran día.");
       break;
     case 0:
-      console.log("¡Hasta luego! Cuídate.");
+      alert("¡Hasta luego! Cuídate.");
       break;
     default:
-      console.log("Por favor, lee bien las opciones.");
+      alert("Por favor, lee bien las opciones.");
   }
 } while (opc !== 0 && opc !== 2 && opc2 !== 7);
